340ba8c2b76a6c3fa5e58a6e381d3227dcd78c74
Collins Igeh <igehac@gmail.com>
2021-03-22 12:34:57 +0100

    created and migrated the posts table, created the posts controller and setup the web routes for posts

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', [PagesController::class, 'index'])->name('home');
 Route::get('/about', [PagesController::class, 'about'])->name('about');
 Route::get('/contact', [PagesController::class, 'contact'])->name('contact');
 Route::get('/services', [PagesController::class, 'services'])->name('services');
+Route::resource('posts', 'App\Http\Controllers\PostsController');
